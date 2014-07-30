67cba089b9fc61602612c61bb488900e28a41fec
Koustubh Shrivatava <koustubh89@gmail.com>
2014-07-30 14:30:37 +0530

    Implementing the fix for logout page

--- app/controllers/api/root_controller.rb
+++ app/controllers/api/root_controller.rb
@@ -1,7 +1,13 @@
 module Api
 	class RootController < ActionController::Base
 	
-  		before_action :authenticate_user!
-	
+  		before_action :authenticate_customised
+	def authenticate_customised
+		if current_user
+			head :ok
+		else
+			head :not_found
+		end
+	end
 	end
 end
